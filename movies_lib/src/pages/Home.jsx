493dabbec81c8545559691be78a8ae03af2ee220
gPerazolli <guilhermeperazolli@gmail.com>
2024-07-02 23:01:34 -0300

    Deploy

---
+++ movies_lib/src/pages/Home.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react"
+import MovieCard from "../components/MovieCard";
+
+import "./MovieGrid.css"
+
+const movieURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const Home = () => {
+
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) =>{
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setTopMovies(data.results);
+  };
+
+  useEffect(() =>{
+    const topRatedURL = `${movieURL}top_rated?${apiKey}`;
+    
+    getTopRatedMovies(topRatedURL);
+  }, [])
+  
+  return (
+
+    <div className="container">
+      <h2 className="title">Melhores Filmes:</h2>
+      <div className="movies-container">
+        {topMovies.length === 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 && topMovies.map((movie) => 
+          <MovieCard key={movie.id} movie={movie}/>
+        )}
+      </div>
+
+    </div>
+  )
+};
+
+export default Home;
